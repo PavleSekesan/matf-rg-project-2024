dac1046bb7e8366d58d5fb49c0f2097802ed6f32
PavleSekesan <pavlesekesan@gmail.com>
2025-10-28 17:54:52 +0100

    Add main

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -1,8 +1,19 @@
 #include <engine/core/Engine.hpp>
+#include <engine/graphics/GraphicsController.hpp>
 
-/**
- * Start here...
- */
-int main(int argc, char** argv) {
-    return 0;
-}
+#include <spdlog/spdlog.h>
+
+#include "GUIController.hpp"
+#include "MainController.hpp"
+
+class MyApp : public engine::core::App {
+public:
+    void app_setup() override {
+        auto main_controller = register_controller<MainController>();
+        auto gui_controller = register_controller<GUIController>();
+        gui_controller->after(main_controller);
+        main_controller->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
+    };
+};
+
+int main(int argc, char **argv) { return std::make_unique<MyApp>()->run(argc, argv); }
